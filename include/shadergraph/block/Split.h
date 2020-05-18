25be606fd8bb1516134d80787cd19a1303819a47
zz <xzrunner@gmail.com>
2020-05-19 05:32:31 +0800

    [FIXED] Split input

--- include/shadergraph/block/Split.h
+++ include/shadergraph/block/Split.h
@@ -13,7 +13,9 @@ public:
     Split()
     {
         SetupPorts({
-            { VarType::Float4, "in0" },
+            { VarType::Float2, "rg" },
+            { VarType::Float3, "rgb" },
+            { VarType::Float4, "rgba" },
         }, {
             { VarType::Float, "r" },
             { VarType::Float, "g" },
@@ -21,6 +23,13 @@ public:
             { VarType::Float, "a" }
         });
     }
+
+    enum class Input
+    {
+        RG,
+        RGB,
+        RGBA,
+    };
 
     enum class Output
     {
@@ -33,18 +42,42 @@ public:
     virtual std::string GetBody() const override
     {
 		std::string ret;
-		if (!m_exports[static_cast<int>(Output::R)].conns.empty()) {
-			ret += "float #r# = #in0#.r;\n";
-		}
-		if (!m_exports[static_cast<int>(Output::G)].conns.empty()) {
-			ret += "float #g# = #in0#.g;\n";
-		}
-		if (!m_exports[static_cast<int>(Output::B)].conns.empty()) {
-			ret += "float #b# = #in0#.b;\n";
-		}
-		if (!m_exports[static_cast<int>(Output::A)].conns.empty()) {
-			ret += "float #a# = #in0#.a;\n";
-		}
+        if (!m_imports[static_cast<int>(Input::RGBA)].conns.empty())
+        {
+		    if (!m_exports[static_cast<int>(Output::R)].conns.empty()) {
+			    ret += "float #r# = #rgba#.r;\n";
+		    }
+		    if (!m_exports[static_cast<int>(Output::G)].conns.empty()) {
+			    ret += "float #g# = #rgba#.g;\n";
+		    }
+		    if (!m_exports[static_cast<int>(Output::B)].conns.empty()) {
+			    ret += "float #b# = #rgba#.b;\n";
+		    }
+		    if (!m_exports[static_cast<int>(Output::A)].conns.empty()) {
+			    ret += "float #a# = #rgba#.a;\n";
+		    }
+        }
+        else if (!m_imports[static_cast<int>(Input::RGB)].conns.empty())
+        {
+            if (!m_exports[static_cast<int>(Output::R)].conns.empty()) {
+                ret += "float #r# = #rgb#.r;\n";
+            }
+            if (!m_exports[static_cast<int>(Output::G)].conns.empty()) {
+                ret += "float #g# = #rgb#.g;\n";
+            }
+            if (!m_exports[static_cast<int>(Output::B)].conns.empty()) {
+                ret += "float #b# = #rgb#.b;\n";
+            }
+        }
+        else if (!m_imports[static_cast<int>(Input::RG)].conns.empty())
+        {
+            if (!m_exports[static_cast<int>(Output::R)].conns.empty()) {
+                ret += "float #r# = #rg#.r;\n";
+            }
+            if (!m_exports[static_cast<int>(Output::G)].conns.empty()) {
+                ret += "float #g# = #rg#.g;\n";
+            }
+        }
 		return ret;
     }
 
